bae7d422950ac74c053ba336b00fafc51870f7ec
Hassan Kandil <hassanosama@aucegypt.edu>
2021-06-08 02:07:24 +0200

    Fixed url trailing slash issue

--- server/loans/urls.py
+++ server/loans/urls.py
@@ -2,8 +2,16 @@ from django.db import router
 from rest_framework import routers
 from .api import LoanViewSet, LoanFundViewSet
 
-router = routers.DefaultRouter()
+from rest_framework.routers import DefaultRouter
+
+
+class CustomDefaultRouter(DefaultRouter):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.trailing_slash = '/?'
+
+router = CustomDefaultRouter()
 router.register('api/loans', LoanViewSet, 'loans')
-router.register('api/loanfunds', LoanFundViewSet, 'loanfunds')
+router.register('api/loan-funds', LoanFundViewSet, 'loan-funds')
 
 urlpatterns = router.urls
